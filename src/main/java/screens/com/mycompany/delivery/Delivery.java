bf05a42aa4315cdfff682913d8c1b56ca6455a01
Zettai <benjaminmachado@rede.ulbra.br>
2026-09-24 01:31:49 +0000

    criandoastelasjava

---
+++ src/main/java/screens/com/mycompany/delivery/Delivery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.delivery;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Delivery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
